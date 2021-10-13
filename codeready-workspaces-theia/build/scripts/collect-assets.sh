cb34190e66bcffe3403163937484c7942a4c3cf5
Samantha Dawley <samantha.m.dawley@gmail.com>
2021-10-13 09:42:09 -0700

    fix: traefik in copied code should be theia

--- codeready-workspaces-theia/build/scripts/collect-assets.sh
+++ codeready-workspaces-theia/build/scripts/collect-assets.sh
@@ -378,7 +378,7 @@ echo
 if [[ ! -x ./uploadAssetsToGHRelease.sh ]]; then 
     curl -sSLO "https://raw.githubusercontent.com/redhat-developer/codeready-workspaces/${MIDSTM_BRANCH}/product/uploadAssetsToGHRelease.sh" && chmod +x uploadAssetsToGHRelease.sh
 fi
-./uploadAssetsToGHRelease.sh -v "${CSV_VERSION}" -b "${MIDSTM_BRANCH}" --prefix traefik "${TARGETDIR}"/asset*
+./uploadAssetsToGHRelease.sh -v "${CSV_VERSION}" -b "${MIDSTM_BRANCH}" --prefix theia "${TARGETDIR}"/asset*
 
 
 if [[ ${COMMIT_CHANGES} -eq 1 ]]; then
